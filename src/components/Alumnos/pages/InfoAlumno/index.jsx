12ad12d4ed2b6e42cc4af53d75d34820b641d490
Jesus0414 <Jesus140400@gmail.com>
2023-09-29 21:04:51 -0700

    cambio para nada
    literalmente

--- src/components/Alumnos/pages/InfoAlumno/index.jsx
+++ src/components/Alumnos/pages/InfoAlumno/index.jsx
@@ -5,24 +5,67 @@ import '..//styles/InfoAlumno.css'
 import { UserOutlined } from '@ant-design/icons';
 import React from 'react';
 import { Avatar, Space } from 'antd';
+import { Badge, Descriptions } from "antd";
+import alumnoImage from "../pages/InfoAlumno/alumnoejemplo.jpg";
 
 
-const InfoAlumno = () => {
-    const [setInformation] = useState('')
-
-    return(<>
-        <Form onFinish={(value) => setInformation(value)}>
-            <Col span={24}>
-                <Space direction="vertical" size={16}>
-                    <Space wrap size={16}>
-                    <Avatar shape="square" size={64} icon={<UserOutlined />} />
-                    </Space>
-                </Space>
-            </Col>
-            
-        </Form>
+const InfoAlumnos = ({ alumnosCard }) => {
+    const items = [
+        {
+          key: '1',
+          label: 'Nombre',
+          children: `${alumnosCard.nombre} ${alumnosCard.apellidos}`,
+        },
+        {
+          key: '2',
+          label: 'Faltas',
+          children: '10',
+        },
+        {
+          key: '3',
+          label: 'Adeudo',
+          children: 'SI (Reportar a caja)',
+        },
+        {
+          key: '4',
+          label: 'Matricula',
+          children: alumnosCard.matricula,
+        },
+        {
+          key: '5',
+          label: 'Usage Time',
+          children: '2019-04-24 18:00:00',
+          span: 2,
+        },
+        {
+          key: '6',
+          label: 'Status',
+          children: <Badge status="processing" text="Running" />,
+          span: 3,
+        },
+        {
+          key: '7',
+          label: 'Foto',
+          children: <img src={alumnoImage} width={190}/>,
+        },
+        {
+          key: '8',
+          label: 'Discount',
+          children: '$20.00',
+        },
+        {
+          key: '9',
+          label: 'Official Receipts',
+          children: '$60.00',
+        },
         
-    </>);
+      ];
+    return ( 
+        <>
+
+            <Descriptions title="Informacion del alumno" bordered items={items} />
+        </>
+     );
 }
  
-export default InfoAlumno;
+export default InfoAlumnos;
