eb934e51e7dc2098276c8fc411dc8d4aeddf2cf4
Gerardo02 <54118434+Gerardo02@users.noreply.github.com>
2023-09-29 23:14:53 -0700

    arreglos menores para mergear

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,17 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Input, Row, Col, Form, Button } from 'antd'
-import './App.css'
-import Dashboard from './Components/Dashboard/Dashboard'
+import Dashboard from './components/Dashboard/Dashboard'
 
 function App() {
   const [information, setInformation] = useState('')
   const navigate = useNavigate();
 
 
-  const ejemplo = () => {
-    console.log(information.Nombre)
-  }
-
   
   return (
     <>
-      <Dashboard/>
-
-      {/* <Form onFinish={(value) => setInformation(value)}>
-        <Row gutter={[10, 5]}>
-          <Col span={12}>
-            <Form.Item
-              name="Nombre"
-            >
-              <Input placeholder='Ruben' />
-
-            </Form.Item>
-            
-          </Col>
-
-          <Col span={12}>
-            <Form.Item
-              name="Apellido"
-            >
-              <Input placeholder='Apellido' />
-            </Form.Item>
-            
-          </Col>
-
-          <Col span={24}>
-            <Form.Item
-              name="Ciudad"
-            >
-              <Input placeholder='Ciudad' />
-            </Form.Item>
-            
-          </Col>
-
-          <Form.Item>
-            <Button htmlType='submit' type='primary'>Submit</Button>
-          </Form.Item>
-
-      
-        </Row>
-
-      </Form> */}
-
-
-      {/* <Button onClick={ejemplo}>Information</Button> */}
-      
+      <Dashboard />
       
     </>
   )
--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -4,80 +4,81 @@ import {
     TeamOutlined,
     EditOutlined,
     UserOutlined
-  } from '@ant-design/icons';
+} from '@ant-design/icons';
+import "../styles/index.css"
 
 
 const { Meta } = Card;
 const { Title } = Typography;
 const Dashboard =() => {
     return (
-        <>
+        <div className='dashboard'>
         {/* <Title>MASTER ACADEMY</Title> */}
-        <img src="src/assets/MasterAcademyLogo.png" alt="Girl in a jacket" width="353" height="123"></img>
-        <Row gutter={[24, 24]}>
-            <Col span={12}>
-                <Card
-                    hoverable
-                    style={{
-                    //width: 420,
-                    color:'white',
-                    backgroundColor: '#FB8B24',
-                    padding: '0.6em',
-                    }}
-                    cover={<DollarOutlined style={{ fontSize: '8vw', backgroundColor: '#FB8B24', marginTop:"40px"}} />}
-                >
-                    <h1 style={{color:'white'}}>Pagos</h1>
-                    <p style={{color:'white'}}>Opciones para el manejo y control de pagos de los alumnos</p>
-                </Card>
-            </Col>
-            <Col span={12}>
-                <Card
-                    hoverable
-                    style={{
+            <img src="src/assets/MasterAcademyLogo.png" alt="Girl in a jacket" width="353" height="123"></img>
+            <Row gutter={[24, 24]}>
+                <Col span={12}>
+                    <Card
+                        hoverable
+                        style={{
                         //width: 420,
                         color:'white',
+                        backgroundColor: '#FB8B24',
                         padding: '0.6em',
-                        backgroundColor: '#E5446D',
-                    }}
-                    cover={<TeamOutlined style={{ fontSize: '8vw', backgroundColor: '#E5446D', marginTop:"40px"  }} />}
-                >
-                    <h1 style={{color:'white'}}>Control Escolar</h1>
-                    <p style={{color:'white'}}>Consulta la información de grupos y alumnos, listas de asistencia, calificaciones, etc. </p>
-                    
-                </Card>
-            </Col>
-            <Col span={12}>
-                <Card
-                    hoverable
-                    style={{
-                        //width: 420,
-                        color:'white',
-                        padding: '0.6em',
-                        backgroundColor: '#04A777',
-                    }}
-                    cover={<EditOutlined style={{ fontSize: '8vw', backgroundColor: '#04A777', marginTop:"40px"  }} />}
-                >
-                    <h1 style={{color:'white'}}>Administración</h1>
-                    <p style={{color:'white'}}>información completa de todos los alumnos, consulta, edita o elimina información</p>
-                </Card>
-            </Col>
-            <Col span={12}>
-                <Card
-                    hoverable
-                    style={{
-                        //width: 420,
-                        color:'white',
-                        padding: '0.6em',
-                        backgroundColor: '#531CB3',
-                    }}
-                    cover={<UserOutlined style={{ fontSize: '8vw', backgroundColor: '#531CB3', marginTop:"40px"  }} />}
-                >
-                    <h1 style={{color:'white'}}>Alumno</h1>
-                    <p style={{color:'white'}}>Accede a tu información básica con tu matricula de alumno</p>
-                </Card>
-            </Col>
-        </Row>
-        </>
+                        }}
+                        cover={<DollarOutlined style={{ fontSize: '8vw', backgroundColor: '#FB8B24', marginTop:"40px"}} />}
+                    >
+                        <h1 style={{color:'white'}}>Pagos</h1>
+                        <p style={{color:'white'}}>Opciones para el manejo y control de pagos de los alumnos</p>
+                    </Card>
+                </Col>
+                <Col span={12}>
+                    <Card
+                        hoverable
+                        style={{
+                            //width: 420,
+                            color:'white',
+                            padding: '0.6em',
+                            backgroundColor: '#E5446D',
+                        }}
+                        cover={<TeamOutlined style={{ fontSize: '8vw', backgroundColor: '#E5446D', marginTop:"40px"  }} />}
+                    >
+                        <h1 style={{color:'white'}}>Control Escolar</h1>
+                        <p style={{color:'white'}}>Consulta la información de grupos y alumnos, listas de asistencia, calificaciones, etc. </p>
+                        
+                    </Card>
+                </Col>
+                <Col span={12}>
+                    <Card
+                        hoverable
+                        style={{
+                            //width: 420,
+                            color:'white',
+                            padding: '0.6em',
+                            backgroundColor: '#04A777',
+                        }}
+                        cover={<EditOutlined style={{ fontSize: '8vw', backgroundColor: '#04A777', marginTop:"40px"  }} />}
+                    >
+                        <h1 style={{color:'white'}}>Administración</h1>
+                        <p style={{color:'white'}}>información completa de todos los alumnos, consulta, edita o elimina información</p>
+                    </Card>
+                </Col>
+                <Col span={12}>
+                    <Card
+                        hoverable
+                        style={{
+                            //width: 420,
+                            color:'white',
+                            padding: '0.6em',
+                            backgroundColor: '#531CB3',
+                        }}
+                        cover={<UserOutlined style={{ fontSize: '8vw', backgroundColor: '#531CB3', marginTop:"40px"  }} />}
+                    >
+                        <h1 style={{color:'white'}}>Alumno</h1>
+                        <p style={{color:'white'}}>Accede a tu información básica con tu matricula de alumno</p>
+                    </Card>
+                </Col>
+            </Row>
+        </div>
       
       
          );
