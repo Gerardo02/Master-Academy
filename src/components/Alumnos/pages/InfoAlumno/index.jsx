5afb4ef39062b6da774bb47e7877705c863e48aa
Jesus0414 <Jesus140400@gmail.com>
2023-09-29 19:54:29 -0700

    InfoDeAlumnosMal
    el inicio del mismo, pero de mala manera.

--- src/components/Alumnos/pages/InfoAlumno/index.jsx
+++ src/components/Alumnos/pages/InfoAlumno/index.jsx
@@ -2,10 +2,27 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Input, Row, Col, Form, Button } from 'antd'
 import '..//styles/InfoAlumno.css'
+import { UserOutlined } from '@ant-design/icons';
+import React from 'react';
+import { Avatar, Space } from 'antd';
+
 
 const InfoAlumno = () => {
-    return ( <>
-    </> );
+    const [setInformation] = useState('')
+
+    return(<>
+        <Form onFinish={(value) => setInformation(value)}>
+            <Col span={24}>
+                <Space direction="vertical" size={16}>
+                    <Space wrap size={16}>
+                    <Avatar shape="square" size={64} icon={<UserOutlined />} />
+                    </Space>
+                </Space>
+            </Col>
+            
+        </Form>
+        
+    </>);
 }
  
 export default InfoAlumno;
--- src/components/Alumnos/pages/alumnos/index.jsx
+++ src/components/Alumnos/pages/alumnos/index.jsx
@@ -11,31 +11,22 @@ function Alumnos() {
         <Form onFinish={(value) => setInformation(value)}>
             <Row gutter={[10, 5]}>
                 <Col span={24}>
-                <Title color='#fff'>Matricula</Title>
-                
+                    <Title color='#fff'>Matricula</Title>
                 </Col>
-
                 <Col span={24}>
                 <Form.Item
                     name="Matricula"
                 >
                     <Input placeholder='ejm. 123456' />
                 </Form.Item>
-                
                 </Col>
-                
                 <Col span={24}>
                     <Form.Item>
                         <Button htmlType='submit' type='primary'>Buscar</Button>
                     </Form.Item>
                 </Col>
-            
             </Row>
-
             </Form>
-
-      
-      
     </> );
 }
  
