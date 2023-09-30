affd124a1ca9b42b094e4d46e014bb71bbee28d5
Gerardo02 <54118434+Gerardo02@users.noreply.github.com>
2023-09-29 23:02:47 -0700

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -14,47 +14,7 @@ function App() {
   return (
     <>
 
-      <Form onFinish={(value) => setInformation(value)}>
-        <Row gutter={[10, 5]}>
-          <Col span={12}>
-            <Form.Item
-              name="Nombre"
-            >
-              <Input placeholder='Nombre' />
-
-            </Form.Item>
-            
-          </Col>
-
-          <Col span={12}>
-            <Form.Item
-              name="Apellido"
-            >
-              <Input placeholder='Apellido' />
-            </Form.Item>
-            
-          </Col>
-
-          <Col span={24}>
-            <Form.Item
-              name="Ciudad"
-            >
-              <Input placeholder='Ciudad' />
-            </Form.Item>
-            
-          </Col>
-
-          <Form.Item>
-            <Button htmlType='submit' type='primary'>Submit</Button>
-          </Form.Item>
-
       
-        </Row>
-
-      </Form>
-
-
-      <Button onClick={ejemplo}>Information</Button>
       
       
     </>
